fb213c3be48e3a1b2b06743f304faa8342af700b
pradeep-08 <114543921+pradeep-08@users.noreply.github.com>
2024-03-11 06:50:39 +0530

    first commit

---
+++ src/Asset/Components/exp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Exp() {
+  return (
+    <div>exp</div>
+  )
+}
+
+export default Exp
---
+++ src/Asset/Components/skills.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../styles/skills.css';
+function Skills() {
+  return <>
+          <div className='skills-main-div'>
+            <div className="skills-div">
+              <div className="intro-description">
+                        Hi, I’m Carmel. Nice to meet you.
+                </div>
+                  <div className='skills-decription'>
+                  <div className="intro-des-2">Extensive 4 years of experience in Android Application Development Developing android applications to enhance and fulfill the user requirements with 
+                    clean and efficient codeMonitoring the performance of live apps and working on optimizing them at the code level.Identifying and resolving bottlenecks, rectifying bugs, and enhancing application performance.
+                  </div>
+                </div>
+               
+            </div>
+          </div>
+          <div className="skills-second-main-div">
+          <div className="skills-second-div"></div>
+          </div>
+          
+  </>
+    
+}
+
+export default Skills
