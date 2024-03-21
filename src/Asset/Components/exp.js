72e0f113c27eea501365a43f69a02eb491bef7f6
pradeep-08 <114543921+pradeep-08@users.noreply.github.com>
2024-03-21 08:01:08 +0530

    updated in Experience page

--- src/Asset/Components/exp.js
+++ src/Asset/Components/exp.js
@@ -9,10 +9,20 @@ function Exp() {
       <div className="exp-main-div">
           
         <div className="experience-section">
-          <div>Program Analyst</div>
-          <div>Mar 2020 - Till date</div>
-          <div> KritiLabs Technologies</div>
+         <div>
+         <div>Program Analyst<span>Mar 2020 - Till date</span></div>
+          <div> KritiLabs Technologies <span>Chennai, Tamil Nadu</span></div>
+         </div>
+        
+          <div className='experience-info'>IOT Technology solutions and managed IOT services for Multiple industries through it's 
+              patented ALS IOT Platform<br></br>Worked with team of 3 members and provided end-to-end solutions for clients<br></br> Lead developer for 3 major mobile applications</div>
+        </div>  
+        <div className='experience-section-2'>
+        <div><div>Software Trainee<span> May 2019 - Feb 2020</span></div>
+          <div> Friendzion Technologies <span>Hosur, Tamil Nadu</span></div></div>
+          <div className='experience-info'>Experienced in working with Android studio and SDK</div>
         </div>
+         </div> 
       </div>
     </>
   )
