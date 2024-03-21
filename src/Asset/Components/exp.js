abb6e15b90c09cd6360c7d36c9bb625f756ca5a6
pradeep-08 <114543921+pradeep-08@users.noreply.github.com>
2024-03-21 08:07:08 +0530

    Update exp.js

--- src/Asset/Components/exp.js
+++ src/Asset/Components/exp.js
@@ -9,17 +9,18 @@ function Exp() {
       <div className="exp-main-div">
           
         <div className="experience-section">
+         <div className="experience-section-1" >
          <div>
-         <div>Program Analyst<span>Mar 2020 - Till date</span></div>
-          <div> KritiLabs Technologies <span>Chennai, Tamil Nadu</span></div>
+         <div className='exp-role'>Program Analyst<span className='time-line-exp'>Mar 2020 - Till date</span></div>
+          <div className='exp-company'> KritiLabs Technologies <span className='time-line-exp'>Chennai, Tamil Nadu</span></div>
          </div>
         
           <div className='experience-info'>IOT Technology solutions and managed IOT services for Multiple industries through it's 
               patented ALS IOT Platform<br></br>Worked with team of 3 members and provided end-to-end solutions for clients<br></br> Lead developer for 3 major mobile applications</div>
         </div>  
         <div className='experience-section-2'>
-        <div><div>Software Trainee<span> May 2019 - Feb 2020</span></div>
-          <div> Friendzion Technologies <span>Hosur, Tamil Nadu</span></div></div>
+        <div><div className='exp-role'>Software Trainee<span className='time-line-exp'> May 2019 - Feb 2020</span></div>
+          <div className='exp-company'> Friendzion Technologies <span className='time-line-exp'>Hosur, Tamil Nadu</span></div></div>
           <div className='experience-info'>Experienced in working with Android studio and SDK</div>
         </div>
          </div> 
