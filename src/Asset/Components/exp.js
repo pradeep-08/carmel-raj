a85fa1f6e6eacb54f434572cc0c96d7a2388daf5
pradeep-08 <114543921+pradeep-08@users.noreply.github.com>
2024-03-26 20:15:04 +0530

    Update exp.js

--- src/Asset/Components/exp.js
+++ src/Asset/Components/exp.js
@@ -12,7 +12,7 @@ function Exp() {
          <div className="experience-section-1" >
          <div>
          <div className='exp-role'>Program Analyst<span className='time-line-exp'>Mar 2020 - Till date</span></div>
-          <div className='exp-company'> KritiLabs Technologies <span className='time-line-exp'>Chennai, Tamil Nadu</span></div>
+          <div className='exp-company'> KritiLabs Technologies <span className='time-line-exp'>Chennai, Tamil Nadu.</span></div>
          </div>
         
           <div className='experience-info'>IOT Technology solutions and managed IOT services for Multiple industries through it's 
@@ -20,7 +20,7 @@ function Exp() {
         </div>  
         <div className='experience-section-2'>
         <div><div className='exp-role'>Software Trainee<span className='time-line-exp'> May 2019 - Feb 2020</span></div>
-          <div className='exp-company'> Friendzion Technologies <span className='time-line-exp'>Hosur, Tamil Nadu</span></div></div>
+          <div className='exp-company'> Friendzion Technologies <span className='time-line-exp'>Hosur, Tamil Nadu.</span></div></div>
           <div className='experience-info'>Experienced in working with Android studio and SDK</div>
         </div>
          </div> 
