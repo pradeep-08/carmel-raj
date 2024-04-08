de61d441210bcc83bcdcfc7a8368ff51e6f3dc77
pradeep-08 <114543921+pradeep-08@users.noreply.github.com>
2024-04-08 21:38:04 +0530

    update in exp
    exprience is done without responsive

--- src/Asset/Components/exp.js
+++ src/Asset/Components/exp.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import '../styles/exp.css';
-// import SkillsLogoOne from '../images/skillsimg.gif';
+ import ExpLogo from '../images/exp.gif';
 
 function Exp() {
   return (
     <>
-      <div className='exp-name'>Experience</div>
+     
       <div className="exp-main-div">
-          
+      
         <div className="experience-section">
+        <div className='exp-name'>Experience</div>
          <div className="experience-section-1" >
          <div>
          <div className='exp-role'>Program Analyst<span className='time-line-exp'>Mar 2020 - Till date</span></div>
@@ -16,14 +17,19 @@ function Exp() {
          </div>
         
           <div className='experience-info'>IOT Technology solutions and managed IOT services for Multiple industries through it's 
-              patented ALS IOT Platform<br></br>Worked With team of 3 members and provided end-to-end solutions for clients<br></br> Lead developer for 3 major mobile applications</div>
+              patented ALS IOT Platform Worked With team of 3 members and provided end-to-end solutions for clients Lead developer for 3 major mobile applications</div>
         </div>  
+       
         <div className='experience-section-2'>
+        <div className='exp-name'>Internship</div>
         <div><div className='exp-role'>Software Trainee<span className='time-line-exp'> May 2019 - Feb 2020</span></div>
           <div className='exp-company'> Friendzion Technologies <span className='time-line-exp'>Hosur, Tamil Nadu.</span></div></div>
           <div className='experience-info'>Experienced in working with Android studio and SDK</div>
         </div>
+       
          </div> 
+         <div className='exp-image'><img  src={ExpLogo} alt="" /></div>
+         
       </div>
     </>
   )
--- src/Asset/Components/intro.js
+++ src/Asset/Components/intro.js
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import '../styles/intro.css';
 import IntroLogo from '../images/developer-icon.png';
 import IntroLogoTwo from '../images/introtarsh.gif';
